dc7baf7dd1af593abf0160591977e4bd73bd36bf
collab-rishi <convey.rishi@gmail.com>
2026-03-23 21:02:06 +0530

    feat(service): add logic to create secondary contacts for new info on existing users

--- src/services/identity.service.ts
+++ src/services/identity.service.ts
@@ -1,5 +1,5 @@
 import { ContactRepository } from '../repositories/contact.repository';
-
+import { Contact } from '@prisma/client';
 export class IdentityService {
   private contactRepo: ContactRepository;
 
@@ -21,16 +21,64 @@ export class IdentityService {
         linkedId: null,
       });
 
-      return {
-        contact: {
-          primaryContactId: newContact.id,
-          emails: [newContact.email].filter(Boolean),
-          phoneNumbers: [newContact.phoneNumber].filter(Boolean),
-          secondaryContactIds: [],
-        },
-      };
+      return this.formatResponse(newContact, [newContact]);
     }
 
-    return null;
+    const primaryIds = [
+      ...new Set(
+        existingContacts.map((c) =>
+          c.linkPrecedence === 'primary' ? c.id : (c.linkedId as number)
+        )
+      ),
+    ];
+
+    const allRelated = await this.contactRepo.findByPrimaryIds(primaryIds);
+
+    const truePrimary = [...allRelated].sort(
+      (a, b) => a.createdAt.getTime() - b.createdAt.getTime()
+    )[0];
+
+    const isNewEmail = email && !allRelated.some((c) => c.email === email);
+    const isNewPhone =
+      phoneNumber && !allRelated.some((c) => c.phoneNumber === phoneNumber);
+
+    if (isNewEmail || isNewPhone) {
+      const newSecondary = await this.contactRepo.create({
+        email,
+        phoneNumber,
+        linkedId: truePrimary.id,
+        linkPrecedence: 'secondary',
+      });
+
+      allRelated.push(newSecondary);
+    }
+
+    return this.formatResponse(truePrimary, allRelated);
+  }
+
+  private formatResponse(primaryContact: Contact, allRelated: Contact[]) {
+    const emails = [
+      ...new Set([primaryContact.email, ...allRelated.map((c) => c.email)]),
+    ].filter(Boolean);
+
+    const phoneNumbers = [
+      ...new Set([
+        primaryContact.phoneNumber,
+        ...allRelated.map((c) => c.phoneNumber),
+      ]),
+    ].filter(Boolean);
+
+    const secondaryContactIds = allRelated
+      .filter((c) => c.linkPrecedence === 'secondary')
+      .map((c) => c.id);
+
+    return {
+      contact: {
+        primaryContactId: primaryContact.id,
+        emails,
+        phoneNumbers,
+        secondaryContactIds,
+      },
+    };
   }
 }
